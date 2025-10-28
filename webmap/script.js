bfc1b3ddf2b5b7577350873a0f4416085511bd7e
Anette Brecko <112785015+anette-brecko@users.noreply.github.com>
2025-10-27 17:26:47 -0700

    Delete webmap directory

--- webmap/script.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWJyZWNrbyIsImEiOiJjbWg5cmczNnAwdXBlMnFvbnJtdXZiN3VoIn0.r0GmwIPsEyZikOfrHNzMkA';
-
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  center: [-122.265, 37.870], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 11.94, 
-  style: 'mapbox://styles/abrecko/cmh9rn7q700qh01r5g902bxcz',
-    });
